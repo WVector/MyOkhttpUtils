8b91644305125f6639d2f26046118fcb4d97dd29
WVector <1964414877@qq.com>
2017-07-25 16:17:14 +0800

    整理

--- app/src/main/java/com/vector/myokhttputils/App.java
+++ app/src/main/java/com/vector/myokhttputils/App.java
@@ -3,7 +3,6 @@ package com.vector.myokhttputils;
 import android.app.Application;
 
 import com.zhy.http.okhttp.OkHttpUtils;
-import com.zhy.http.okhttp.builder.HttpParams;
 
 /**
  * Created by Vector
@@ -15,11 +14,8 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
-
-        HttpParams httpParams = new HttpParams();
-        httpParams.addParam("key1", "value1");
-
-        OkHttpUtils.getInstance().init(this)
+        OkHttpUtils.getInstance()
+                .init(this)
                 .debug(true, "okhttp")
                 .timeout(6 * 1000L)
                 .setGlobalParams(new MyGlobalParams());
